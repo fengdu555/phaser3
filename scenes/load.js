39bac2db19247b35b006c1e8bc848b5da95f7cc2
xiaowei <lixiaowei@chinamuxie.com>
2020-06-10 14:58:59 +0800

    入门指南学习01场景区分

---
+++ scenes/load.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-10 11:44:31
+ * @LastEditTime: 2020-06-10 14:44:39
+ * @LastEditors: Please set LastEditors
+ * @Description: 加载场景
+ * @FilePath: /github/scenes/load.js
+ */ 
+class LoadScene extends Phaser.Scene{
+  constructor(){
+      super("LoadScene");
+  }
+  preload(){
+    this.load.setBaseURL('assets/');
+
+    this.load.image('sky', 'skies/space3.png');
+    this.load.image('logo', 'sprites/phaser3-logo.png');
+    this.load.image('red', 'particles/red.png');
+  }
+  create(){
+
+    this.add.image(400, 300, 'sky');
+
+        var particles = this.add.particles('red');
+
+        var emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
+
+        var logo = this.physics.add.image(400, 100, 'logo');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        emitter.startFollow(logo);
+  }
+}
